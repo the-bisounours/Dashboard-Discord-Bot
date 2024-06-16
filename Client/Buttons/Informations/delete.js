63b81db3f71f3421f835abfbbd44fe863d60217d
the_bisounours <sedorne.jayson@gmail.com>
2024-06-16 15:12:59 +0200

    Correction d'une erreur pour supprimer un message en Dm.

--- Client/Buttons/Informations/delete.js
+++ Client/Buttons/Informations/delete.js
@@ -1,4 +1,4 @@
-const {Client, ButtonInteraction } = require("discord.js");
+const { Client, ButtonInteraction } = require("discord.js");
 
 module.exports = {
     id: "delete",
@@ -11,6 +11,12 @@ module.exports = {
     execute: async (client, interaction) => {
 
         await interaction.update({ fetchReply: true });
-        return await interaction.message.delete();
+        if (interaction.guild) {
+            return await interaction.message.delete();
+        } else {
+            const channel = await client.channels.fetch(interaction.channelId);
+            const message = await channel.messages.fetch(interaction.message.id);
+            await message.delete();
+        };
     }
 };
