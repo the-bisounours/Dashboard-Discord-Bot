1707f1c4d2823ef5787d714ea3c1a730698d5424
the_bisounours <sedorne.jayson@gmail.com>
2024-06-16 14:09:39 +0200

    Gestion des erreurs pour la base de donnée.

--- Client/Handlers/mongoose.js
+++ Client/Handlers/mongoose.js
@@ -8,7 +8,12 @@ const mongoose = require("mongoose");
 module.exports = (client) => {
     
     mongoose.connect(process.env.mongoDB);
+
     mongoose.connection.on("connected", () => {
         console.log("\x1b[36m", "La base de donnée est connectée.");
+    });
+
+    mongoose.connection.on("error", (err) => {
+        console.error("Erreur de connexion à la base de données:", err);
     });
 };
