c899388b196accc34f739a893d512bc2079f2c4b
the_bisounours <sedorne.jayson@gmail.com>
2024-06-16 13:53:52 +0200

    Ajout de l'handler pour les évènements

---
+++ Client/Handlers/events.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+module.exports = async (client) => {
+    
+
+    const loadEvents = async (dir) => {
+        const events = fs.readdirSync(`./Client/Events/${dir}`).filter(file => file.endsWith(".js"));
+
+        for (const file of events) {
+            const event = require(`../Events/${dir}/${file}`);
+
+            if (event.once) {
+                client.once(event.name, (...args) => event.execute(client, ...args));
+            } else {
+                client.on(event.name, (...args) => event.execute(client, ...args));
+            };
+        };
+    };
+
+    for (const dir of fs.readdirSync("./Client/Events")) {
+        await loadEvents(dir);
+    };
+
+    console.log("\x1b[32m", "Les évènements ont été mis en place avec succès !");
+};
