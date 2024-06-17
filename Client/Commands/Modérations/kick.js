02f843a673c653488450939fae8bfe9570cdd7a4
the_bisounours <sedorne.jayson@gmail.com>
2024-06-17 18:22:00 +0200

    Commande kick avec preuve.

---
+++ Client/Commands/Modérations/kick.js
@@ -0,0 +1,118 @@
+const { SlashCommandBuilder, PermissionFlagsBits, Client, ChatInputCommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("kick")
+        .setDescription("Permet d'exclure un utilisateur du serveur discord.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+        .addUserOption(option => option
+            .setName("membre")
+            .setDescription("Le membre à exclure du serveur discord.")
+            .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName("raison")
+            .setDescription("La raison de l'exclusion du serveur discord.")
+            .setRequired(false)
+            .setMaxLength(200)
+            .setMinLength(1)
+        )
+        .addAttachmentOption(option => option
+            .setName("preuve")
+            .setDescription("La preuve de l'exclusion du serveur discord.")
+            .setRequired(false)
+        ),
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const user = interaction.options.getUser("membre") ? interaction.options.getUser("membre") : interaction.user;
+        const member = interaction.guild.members.cache.get(user.id);
+
+        const raison = interaction.options.getString("raison") ? interaction.options.getString("raison") : "aucune raison";
+
+        if (!member) {
+            return await interaction.reply({
+                content: ":x: Je n'arrive pas a trouver le membre.",
+                ephemeral: true
+            });
+        };
+
+        if (member.user.id === interaction.guild.ownerId) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas exclure propriétaire du serveur discord.",
+                ephemeral: true
+            });
+        };
+
+        if (member.user.id === interaction.user.id) {
+            return await interaction.reply({
+                content: ":x: Pourquoi essayez-vous de vous exclure ?",
+                ephemeral: true
+            });
+        };
+
+        if (member.user.id === client.user.id) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas m'exclure, essayer la commande /leave.",
+                ephemeral: true
+            });
+        };
+
+        if (interaction.user.id !== interaction.guild.ownerId && interaction.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas exclure un membre plus haut que vous.",
+                ephemeral: true
+            });
+        };
+
+        if (interaction.guild.members.me.roles.highest.comparePositionTo(member.roles.highest) <= 0) {
+            return await interaction.reply({
+                content: ":x: Je ne peux pas exclure un membre plus haut que moi.",
+                ephemeral: true
+            });
+        };
+
+        if (!member.kickable) {
+            return await interaction.reply({
+                content: ":x: Je ne peux pas exclure ce membre.",
+                ephemeral: true
+            });
+        };
+
+        if (!member.user.bot) {
+            try {
+                await member.send({
+                    content: `Vous avez été exclus par \`${interaction.user.displayName}\` pour \`${raison}\`.${interaction.options.getAttachment("preuve") ? ` [\`preuve\`](${interaction.options.getAttachment("preuve").url})` : ""}`,
+                    components: [
+                        new ActionRowBuilder()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setStyle(ButtonStyle.Secondary)
+                                    .setLabel(`Envoyé depuis ${interaction.guild.name}`)
+                                    .setDisabled(true)
+                            )
+                    ]
+                });
+            } catch (err) {};
+        };
+
+        return await interaction.guild.members.kick(member.user.id, raison)
+            .then(async kickInfo => {
+                return await interaction.reply({
+                    content: `Vous avez exclus \`${kickInfo.username}\` pour \`${raison}\`.${interaction.options.getAttachment("preuve") ? ` [\`preuve\`](${interaction.options.getAttachment("preuve").url})` : ""}`,
+                });
+            })
+            .catch(async err => {
+                return await interaction.reply({
+                    content: `\`${member.user.displayName}\` n'a pas été exclus à cause d'une erreur.`,
+                    ephemeral: true
+                });
+            });
+    }
+};
