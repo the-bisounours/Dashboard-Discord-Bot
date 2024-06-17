0230e43435f6096a0b85b2b69f1c521a024b1435
the_bisounours <sedorne.jayson@gmail.com>
2024-06-17 19:07:38 +0200

    Commande leave pour faire quitter le robot.

---
+++ Client/Commands/Modérations/leave.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, PermissionFlagsBits, Client, ChatInputCommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("leave")
+        .setDescription("Permet de faire quitter le robot du serveur discord.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        await interaction.reply({
+            content: `Au revoir ! 👋`,
+            ephemeral: true
+        });
+
+        return await interaction.guild.leave()
+            .then(async guild => {
+                try {
+                    return await interaction.member.send({
+                        content: `J'ai quitter le serveur \`${guild.name}\``,
+                        components: [
+                            new ActionRowBuilder()
+                                .addComponents(
+                                    new ButtonBuilder()
+                                        .setStyle(ButtonStyle.Secondary)
+                                        .setLabel(`Envoyé depuis ${interaction.guild.name}`)
+                                        .setDisabled(true)
+                                )
+                        ]
+                    });
+                } catch (err) {};
+            })
+            .catch(async err => {
+                return await interaction.editReply({
+                    content: `Je n'ai pas quitter le serveur à cause d'une erreur.`,
+                    ephemeral: true
+                });
+            });
+    }
+};
