19dc12b74aa74e3168a9c68df4040de0fe49b88c
the_bisounours <sedorne.jayson@gmail.com>
2024-06-18 17:44:30 +0200

    Ajout de la commande untimeout.

---
+++ Client/Commands/Modérations/untimeout.js
@@ -0,0 +1,120 @@
+const { SlashCommandBuilder, PermissionFlagsBits, Client, ChatInputCommandInteraction, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const { convert } = require("../../Functions/convert");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("untimeout")
+        .setDescription("Permet de rendre la parole à un utilisateur du serveur discord.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+        .addUserOption(option => option
+            .setName("membre")
+            .setDescription("Le membre à rendre la parole du serveur discord.")
+            .setRequired(true)
+        )
+        .addStringOption(option => option
+            .setName("raison")
+            .setDescription("La raison de la parole rendu du membre.")
+            .setRequired(false)
+            .setMaxLength(200)
+            .setMinLength(1)
+        )
+        .addAttachmentOption(option => option
+            .setName("preuve")
+            .setDescription("La preuve de la parole rendu du membre.")
+            .setRequired(false)
+        ),
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const user = interaction.options.getUser("membre") ? interaction.options.getUser("membre") : interaction.user;
+        const member = interaction.guild.members.cache.get(user.id);
+
+        const raison = interaction.options.getString("raison") ? interaction.options.getString("raison") : "aucune raison";
+
+        if (!member) {
+            return await interaction.reply({
+                content: ":x: Je n'arrive pas a trouver le membre.",
+                ephemeral: true
+            });
+        };
+
+        if (!member.isCommunicationDisabled()) {
+            return await interaction.reply({
+                content: ":x: Le membre à la possibilité de parler.",
+                ephemeral: true
+            });
+        };
+
+
+        if (member.user.id === interaction.guild.ownerId) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas rendre la parole propriétaire du serveur discord.",
+                ephemeral: true
+            });
+        };
+
+        if (member.user.id === interaction.user.id) {
+            return await interaction.reply({
+                content: ":x: Pourquoi essayez-vous de vous rendre la parole ?",
+                ephemeral: true
+            });
+        };
+
+        if (member.user.id === client.user.id) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas me rendre la parole, essayer la commande /leave.",
+                ephemeral: true
+            });
+        };
+
+        if (interaction.user.id !== interaction.guild.ownerId && interaction.member.roles.highest.comparePositionTo(member.roles.highest) <= 0) {
+            return await interaction.reply({
+                content: ":x: Vous ne pouvez pas rendre la parole un membre plus haut que vous.",
+                ephemeral: true
+            });
+        };
+
+        if (interaction.guild.members.me.roles.highest.comparePositionTo(member.roles.highest) <= 0) {
+            return await interaction.reply({
+                content: ":x: Je ne peux pas rendre la parole un membre plus haut que moi.",
+                ephemeral: true
+            });
+        };
+
+        if (!member.user.bot) {
+            try {
+                await member.send({
+                    content: `Vous avez été rendu de parole par \`${interaction.user.displayName}\` pour \`${raison}\`.${interaction.options.getAttachment("preuve") ? ` [\`preuve\`](${interaction.options.getAttachment("preuve").url})` : ""}`,
+                    components: [
+                        new ActionRowBuilder()
+                            .addComponents(
+                                new ButtonBuilder()
+                                    .setStyle(ButtonStyle.Secondary)
+                                    .setLabel(`Envoyé depuis ${interaction.guild.name}`)
+                                    .setDisabled(true)
+                            )
+                    ]
+                });
+            } catch (err) {};
+        };
+
+        return await member.timeout(null, raison)
+            .then(async timeoutInfo => {
+                return await interaction.reply({
+                    content: `Vous avez rendu la parole à \`${timeoutInfo.user.displayName}\` pour \`${raison}\`.${interaction.options.getAttachment("preuve") ? ` [\`preuve\`](${interaction.options.getAttachment("preuve").url})` : ""}`,
+                });
+            })
+            .catch(async err => {
+                return await interaction.reply({
+                    content: `\`${member.user.displayName}\` n'a pas été rendu de parole à cause d'une erreur.`,
+                    ephemeral: true
+                });
+            });
+    }
+};
