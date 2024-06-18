17e514d5ca26ec6ac77137204268844249bc26b1
the_bisounours <sedorne.jayson@gmail.com>
2024-06-18 19:15:42 +0200

    Amelioration de la fonction de convertion.

--- Client/Functions/convert.js
+++ Client/Functions/convert.js
@@ -1,25 +1,43 @@
 
 /**
  * 
- * @param {Number} ms 
+ * @param {Number} number 
+ * @param {String} unit 
  * @returns {String}
  */
-module.exports = ms => {
+module.exports = (number, unit) => {
+
+    switch (unit) {
+        case 'millisecondes':
+            number;
+        case 'secondes':
+            number = number * 1000;
+        case 'mimutes':
+            number = number * 1000 * 60;
+        case 'heures':
+            number = number * 1000 * 60 * 60;
+        case 'jours':
+            number = number * 1000 * 60 * 60 * 24;
+        default:
+            number;
+        break;
+    };
+
     const day = 24 * 60 * 60 * 1000;
     const hour = 60 * 60 * 1000;
     const minute = 60 * 1000;
     const second = 1000;
 
-    const days = Math.floor(ms / day);
-    ms -= days * day;
+    const days = Math.floor(number / day);
+    number -= days * day;
 
-    const hours = Math.floor(ms / hour);
-    ms -= hours * hour;
+    const hours = Math.floor(number / hour);
+    number -= hours * hour;
 
-    const minutes = Math.floor(ms / minute);
-    ms -= minutes * minute;
+    const minutes = Math.floor(number / minute);
+    number -= minutes * minute;
 
-    const seconds = Math.floor(ms / second);
+    const seconds = Math.floor(number / second);
 
     let result = "";
     if (days > 0) {
