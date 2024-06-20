0558ff55899a8de5a295f03b0a368dbcddb01c9b
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 21:32:48 +0200

    Commande back.

---
+++ Client/Commands/Musiques/back.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("back")
+        .setDescription("Permet de revenir en arrière.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(null)
+        .addBooleanOption(option => option
+            .setName("préserver-actuel")
+            .setDescription("Permet de préserver la musique actuelle.")
+            .setRequired(false)
+        ),
+
+    category: "Musiques",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const player = useMainPlayer();
+
+        const channel = interaction.member.voice?.channel;
+        if (!channel) {
+            return await interaction.reply({
+                content: "Vous n'êtes connecté à aucun canal vocal.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id) || !player.queues.get(interaction.guild.id).isPlaying()) {
+            return await interaction.reply({
+                content: "Il n'y a pas de musique en cours.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id).history.previousTrack) {
+            return await interaction.reply({
+                content: "Il n'y a pas eu de musique avant celle-ci.",
+                ephemeral: true
+            });
+        };
+        
+        
+        const previousTrack = player.queues.get(interaction.guild.id).history.previousTrack
+        player.queues.get(interaction.guild.id).history.back(interaction.options.getBoolean("préserver-actuel"))
+        return await interaction.reply({
+            content: `La musique \`${previousTrack.title}\` vient d'être rejouer.`
+        });
+    }
+};
