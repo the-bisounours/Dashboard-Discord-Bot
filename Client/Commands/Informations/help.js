9866f1f846656e3d11622091fc2a8e22e120c894
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 11:16:34 +0200

    Commande help.

--- Client/Commands/Informations/help.js
+++ Client/Commands/Informations/help.js
@@ -37,42 +37,11 @@ module.exports = {
 
     execute: async (client, interaction) => {
 
-        if (!interaction.options.getString("commande")) {
-            const activCmds = client.commands.map(cmd => interaction.member.permissions.has(new PermissionsBitField(cmd.permission)));
-            const sortie = activCmds.filter(c => c === true);
-
-            const categories = [];
-            client.commands.forEach(command => {
-                if (!categories.includes(command.category)) categories.push(command.category);
-            });
-
-            const embed = new EmbedBuilder()
-                .setColor("Blurple")
-                .setTitle(`Informations des commandes du robot`)
-                .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
-                .setDescription(`Commandes du robot : \`${client.commands.size}\`\nCommandes disponibles pour ${interaction.member} : \`${sortie.length}\``)
-                .setTimestamp()
-                .setFooter({ text: `${client.user.username} - help`, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
-
-            categories.sort().forEach(async category => {
-                const commands = client.commands.filter(cmd => cmd.category === category);
-                const categoryEmpty = commands.map(cmd => interaction.member.permissions.has(new PermissionsBitField(cmd.permission)));
-
-                if (categoryEmpty.every(element => element === false)) return;
-                embed.addFields({
-                    name: `${category}`, value: `>>> ${commands.map(cmd => `${interaction.member.permissions.has(new PermissionsBitField(cmd.permission)) ? `\`${cmd.data.name}\`: ${cmd.data.description}\n` : ""}`).join("")}`
-                });
-            });
-
-            return await interaction.reply({ 
-                embeds: [embed],
-            });
-        } else {
-
+        if(interaction.options.getString("commande")) {
             const command = client.commands.filter(command => command.data.name === interaction.options.getString("commande")).first()
             if (!command) {
                 return await interaction.reply({
-                    content: `Impossible de trouver la commande: \`${interaction.options.getString("commande")}\``,
+                    content: `:x: Impossible de trouver la commande: \`${interaction.options.getString("commande")}\``,
                     ephemeral: true
                 });
             };
@@ -90,6 +59,36 @@ module.exports = {
                         .setTimestamp()
                         .setDescription(`> **Nom:** \`${command.data.name}\`\n> **Description:** \`${command.data.description}\`\n> **Catégorie:** \`${command.category}\`\n> **Prémium:** \`${command.premium ? 'oui' : 'non'}\`\n> **Message privé:** \`${command.data.dm_permission ? 'oui' : 'non'}\`\n> **NSFW:** \`${command.data.nsfw ? 'oui' : 'non'}\`\n> **Permissions Utilisateur:** \`${PermissionFlags(command.data.default_member_permissions)}\``)
                 ]
+            });
+        } else {
+            const viewcommands = client.commands.map(cmd => interaction.member.permissions.has(new PermissionsBitField(cmd.permission)));
+            const cmds = viewcommands.filter(c => c === true);
+
+            const categories = [];
+            client.commands.forEach(command => {
+                if (!categories.includes(command.category)) categories.push(command.category);
+            });
+
+            const embed = new EmbedBuilder()
+                .setColor("Blurple")
+                .setTitle(`Informations des commandes du robot`)
+                .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
+                .setDescription(`Commandes du robot : \`${client.commands.size}\`\nCommandes disponibles pour ${interaction.member} : \`${cmds.length}\``)
+                .setTimestamp()
+                .setFooter({ text: `${client.user.username} - help`, iconURL: client.user.displayAvatarURL({ dynamic: true }) });
+
+            categories.sort().forEach(async category => {
+                const commands = client.commands.filter(cmd => cmd.category === category);
+                const categoryEmpty = commands.map(cmd => interaction.member.permissions.has(new PermissionsBitField(cmd.permission)));
+
+                if (categoryEmpty.every(element => element === false)) return;
+                embed.addFields({
+                    name: `${category}`, value: `>>> ${commands.map(cmd => `${interaction.member.permissions.has(new PermissionsBitField(cmd.permission)) ? `\`${cmd.data.name}\`: ${cmd.data.description}\n` : ""}`).join("")}`
+                });
+            });
+
+            return await interaction.reply({ 
+                embeds: [embed],
             });
         };
     }
