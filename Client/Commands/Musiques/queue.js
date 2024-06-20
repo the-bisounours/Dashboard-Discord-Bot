47fd7ad1f2a3e53cb93649df590599b9894fc57b
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 20:05:08 +0200

    Ajout de la commande queue.

--- Client/Commands/Musiques/queue.js
+++ Client/Commands/Musiques/queue.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder, Client, ChatInputCommandInteraction } = require("discord.js");
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction, EmbedBuilder } = require("discord.js");
+const paginations = require("../../Functions/paginations");
 const { useMainPlayer } = require('discord-player');
 
 module.exports = {
@@ -6,12 +7,7 @@ module.exports = {
         .setName("queue")
         .setDescription("Permet de regarde la liste.")
         .setDMPermission(false)
-        .setDefaultMemberPermissions(null)
-        .addStringOption(option => option
-            .setName("song")
-            .setDescription("La musique à jouer dans le salon vocal.")
-            .setRequired(true)
-        ),
+        .setDefaultMemberPermissions(null),
 
     category: "Musiques",
 
@@ -32,6 +28,32 @@ module.exports = {
             });
         };
 
-        
+        if (!player.queues.get(interaction.guild.id) || !player.queues.get(interaction.guild.id).isPlaying()) {
+            return await interaction.reply({
+                content: "Il n'y a pas de musique en cours.",
+                ephemeral: true
+            });
+        };
+
+        const embeds = [];
+        const tracks = player.queues.get(interaction.guild.id).tracks.toArray()
+
+        for (let index = 0; index < tracks.length; index++) {
+            const track = tracks[index]
+            embeds.push(
+                new EmbedBuilder()
+                .setTitle("Information de la liste des musiques")
+                .setColor("Blurple")
+                .setFooter({
+                    text: client.user.displayName,
+                    iconURL: client.user.displayAvatarURL()
+                })
+                .setTimestamp()
+                .setThumbnail(track.thumbnail)
+                .setDescription(`> **Titre:** \`${track.title}\`\n> **Durée:** \`${track.duration}\`\n> **Auteur:** \`${track.author}\`\n> **View${track.views > 0 ? "s" : ""}:** \`${track.views}\`\n> **Ajouté par:** \`${track.requestedBy.displayName}\`\n> **Playlist:** \`${track.playlist ? "oui" : "non"}\``)
+            )
+        };
+
+        return await paginations(interaction, embeds, 60 * 1000, false);
     }
 };
