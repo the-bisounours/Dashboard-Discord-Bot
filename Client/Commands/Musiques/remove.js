635b2e5d08bbf6a0467f128ab77a067ea94791c4
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 13:45:30 +0200

    Commande remove musique.

---
+++ Client/Commands/Musiques/remove.js
@@ -0,0 +1,77 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction, AutocompleteInteraction } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("remove")
+        .setDescription("Permet de retirer la musique.")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(null)
+        .addStringOption(option => option
+            .setName("song")
+            .setDescription("Permet de retirer la musique")
+            .setAutocomplete(true)
+            .setRequired(true)
+        ),
+
+    category: "Musiques",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {AutocompleteInteraction} interaction 
+     */
+    autocomplete: async (client, interaction) => {
+
+        const player = useMainPlayer();
+        const tracks = player.queues.get(interaction.guild.id).tracks
+
+        const focusedValue = interaction.options.getFocused();
+        const filtered = tracks.data.filter(choice => choice.title.startsWith(focusedValue));
+        await interaction.respond(filtered.map(choice =>
+            ({ 
+                name: choice.title, 
+                value: choice.id
+            })
+        ));
+    },
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const player = useMainPlayer();
+
+        const channel = interaction.member.voice?.channel;
+        if (!channel) {
+            return await interaction.reply({
+                content: "Vous n'êtes connecté à aucun canal vocal.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id) || !player.queues.get(interaction.guild.id).isPlaying()) {
+            return await interaction.reply({
+                content: "Il n'y a pas de musique en cours.",
+                ephemeral: true
+            });
+        };
+
+        const track = player.queues.get(interaction.guild.id).tracks.find(song => song.id === interaction.options.getString("song"));
+        if (!track) {
+            return await interaction.reply({
+                content: "Je n'arrive pas a trouver la musique.",
+                ephemeral: true
+            });
+        };
+        
+        player.queues.get(interaction.guild.id).node.remove(currentTrack);
+
+        return await interaction.reply({
+            content: `La musique \`${currentTrack.title}\` a été retiré.`
+        });
+    }
+};
