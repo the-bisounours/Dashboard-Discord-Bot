42fc494a663af83a4b3415652d4fea18a2efd7c6
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 13:09:48 +0200

    Commande pause et resume.

---
+++ Client/Commands/Musiques/resume.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("resume")
+        .setDescription("Permet de mettre la musique.")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(null),
+
+    category: "Musiques",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const player = useMainPlayer();
+
+        const channel = interaction.member.voice?.channel;
+        if (!channel) {
+            return await interaction.reply({
+                content: "Vous n'êtes connecté à aucun canal vocal.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id) || !player.queues.get(interaction.guild.id).isPlaying()) {
+            return await interaction.reply({
+                content: "Il n'y a pas de musique en cours.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id).node.isPaused()) {
+            return await interaction.reply({
+                content: "La musique est déjà entrain de jouer.",
+                ephemeral: true
+            });
+        };
+
+        const currentTrack = player.queues.get(interaction.guild.id).currentTrack;
+        player.queues.get(interaction.guild.id).node.resume(true);
+        return await interaction.reply({
+            content: `La musique \`${currentTrack.title}\` a été remis.`
+        });
+    }
+};
