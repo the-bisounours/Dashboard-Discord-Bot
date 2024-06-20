25ce9a51a52cfa677524b5d1a65f424c142ba07e
the_bisounours <sedorne.jayson@gmail.com>
2024-06-20 12:36:44 +0200

    Commande stop.

---
+++ Client/Commands/Musiques/stop.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction } = require("discord.js");
+const { useMainPlayer } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("stop")
+        .setDescription("Permet d'arrêter a la musique.")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(null),
+
+    category: "Musiques",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        const player = useMainPlayer();
+
+        const channel = interaction.member.voice?.channel;
+        if (!channel) {
+            return await interaction.reply({
+                content: "Vous n'êtes connecté à aucun canal vocal.",
+                ephemeral: true
+            });
+        };
+
+        if (!player.queues.get(interaction.guild.id).isPlaying()) {
+            return await interaction.reply({
+                content: "Il n'y a pas de musique en cours.",
+                ephemeral: true
+            });
+        };
+
+        player.queues.get(interaction.guild.id).node.stop();
+        return await interaction.reply({
+            content: `La musique a été stopper.`,
+            ephemeral: true
+        });
+    }
+};
