d15049e8dccf8e8bbc2c8405db2d9714ca8a8ba9
the_bisounours <sedorne.jayson@gmail.com>
2024-06-22 23:52:48 +0200

    Commande uptime.js

---
+++ Client/Commands/Informations/uptime.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction } = require("discord.js");
+const convert = require("../../Functions/convert");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("uptime")
+        .setDescription("Permet de connaitre depuis combien de temps le robot est en ligne.")
+        .setDMPermission(true)
+        .setDefaultMemberPermissions(null),
+
+    category: "Informations",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        return await interaction.reply({
+            content: `Le robot est en ligne depuis ${convert(client.uptime, "millisecondes")}.`,
+            ephemeral: true
+        });
+    }
+};
