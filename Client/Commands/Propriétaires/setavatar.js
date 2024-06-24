3449349543735f51a6ddc635f26f0fe0fb01daa0
the_bisounours <sedorne.jayson@gmail.com>
2024-06-24 11:42:00 +0200

    Configuration de la description/nom du robot.

---
+++ Client/Commands/Propriétaires/setavatar.js
@@ -0,0 +1,66 @@
+const { SlashCommandBuilder, Client, ChatInputCommandInteraction, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder } = require("discord.js");
+const activity = require("../../Functions/activity");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("setavatar")
+        .setDescription("Permet de modifier l'avatar du robot.")
+        .setDMPermission(false)
+        .setDefaultMemberPermissions(null)
+        .addAttachmentOption(option => option
+            .setName("avatar")
+            .setDescription("Permet de modifier l'avatar du robot.")
+            .setRequired(true)
+        ),
+
+    category: "Propriétaires",
+
+    /**
+     * 
+     * @param {Client} client 
+     * @param {ChatInputCommandInteraction} interaction 
+     */
+    execute: async (client, interaction) => {
+
+        if (interaction.user.id !== process.env.ownerId) {
+            return await interaction.reply({
+                content: "Vous n'etes pas le propriétaire du robot.",
+                ephemeral: true
+            });
+        };
+
+        return await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                    .setTitle("Informations du robot")
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setColor("Blurple")
+                    .setFooter({
+                        text: client.user.displayName,
+                        iconURL: client.user.displayAvatarURL()
+                    })
+                    .setTimestamp()
+                    .setDescription(`> **Nom:** \`${client.user.username}\`\n> **Description:** \`${client.application.description ? client.application.description : "Aucune"}\`\n> **Avatar:** ${client.user.avatar ? `[\`clique ici\`](${client.user.displayAvatarURL()})` : "\`Aucun\`"}\n> **Bannière:** ${client.user.banner ? `[\`clique ici\`](${client.user.bannerURL()})` : "\`Aucune\`"}\n> **Satus:** \`${client.user.presence && client.user.presence.status ? client.user.presence.status : "Aucun"}\`\n> **Activité:** \`${client.user.presence && client.user.presence.activities.length > 0 ? `${activity(client.user.presence.activities[0].type)} ${client.user.presence.activities[0].name}` : "Aucune"}\``)
+            ],
+            components: [
+                new ActionRowBuilder()
+                    .addComponents(
+                        new StringSelectMenuBuilder()
+                            .setCustomId("botconfig")
+                            .setDisabled(false)
+                            .setMaxValues(1)
+                            .setMinValues(1)
+                            .setPlaceholder("Config les informations du robot")
+                            .addOptions(
+                                new StringSelectMenuOptionBuilder()
+                                    .setLabel("Modifier le nom")
+                                    .setValue("name"),
+                                new StringSelectMenuOptionBuilder()
+                                    .setLabel("Modifier la description")
+                                    .setValue("description")
+                            )
+                    )
+            ]
+        })
+    }
+};
--- Client/Modals/BotConfig/configName.js
+++ Client/Modals/BotConfig/configName.js
@@ -1,4 +1,5 @@
-const { Client, ModalSubmitInteraction } = require("discord.js");
+const { Client, ModalSubmitInteraction, EmbedBuilder } = require("discord.js");
+const activity = require("../../Functions/activity");
 
 module.exports = {
     id: "configName",
@@ -14,12 +15,29 @@ module.exports = {
 
         return await client.user.setUsername(name)
             .then(async clientInfo => {
+
+                await interaction.message.edit({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle("Informations du robot")
+                            .setThumbnail(clientInfo.client.user.displayAvatarURL())
+                            .setColor("Blurple")
+                            .setFooter({
+                                text: clientInfo.client.user.displayName,
+                                iconURL: clientInfo.client.user.displayAvatarURL()
+                            })
+                            .setTimestamp()
+                            .setDescription(`> **Nom:** \`${clientInfo.client.user.username}\`\n> **Description:** \`${clientInfo.client.application.description ? clientInfo.client.application.description : "Aucune"}\`\n> **Avatar:** ${clientInfo.client.user.avatar ? `[\`clique ici\`](${clientInfo.client.user.displayAvatarURL()})` : "\`Aucun\`"}\n> **Bannière:** ${clientInfo.client.user.banner ? `[\`clique ici\`](${clientInfo.client.user.bannerURL()})` : "\`Aucune\`"}\n> **Satus:** \`${clientInfo.client.user.presence && clientInfo.client.user.presence.status ? clientInfo.client.user.presence.status : "Aucun"}\`\n> **Activité:** \`${clientInfo.client.user.presence && clientInfo.client.user.presence.activities.length > 0 ? `${activity(clientInfo.client.user.presence.activities[0].type)} ${clientInfo.client.user.presence.activities[0].name}` : "Aucune"}\``)
+                    ]
+                });
+
                 return await interaction.reply({
                     content: `Le robot a changé de nom: \`${clientInfo.username}\``,
                     ephemeral: true
                 });
             })
             .catch(async err => {
+                console.log(err)
                 return await interaction.reply({
                     content: `Le robot n'a pas changé de nom à cause d'une erreur.`,
                     ephemeral: true
