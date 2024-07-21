0a535440cc2ad4d1cbb2c16d67088be60777111b
the_bisounours <sedorne.jayson@gmail.com>
2024-07-21 18:54:40 +0200

    styles.css

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -14,6 +14,7 @@ module.exports = client => {
 
     app.set('view engine', 'ejs');
     app.set('views', path.join(__dirname, 'views'));
+    app.use(express.static('public'));
 
     app.locals.client = client;
     
